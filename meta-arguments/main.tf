b1311fc05f65c7939240c5eba170706320916246
sudiptaMukherjee2001 <frontendDeveloperabbs@gmail.com>
2026-04-14 17:18:31 +0530

    learning meta arguments

--- meta-arguments/main.tf
+++ meta-arguments/main.tf
@@ -2,14 +2,16 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "server" {
-  for_each = toset(var.envs)
 
-  ami           = "ami-03ea746da1a2e36e7" # Amazon Linux 2
-  instance_type = "t3.micro"
+#Bucket Creation usign count meta argument
+resource "aws_s3_bucket" "bucket1" {
+  count = length(var.bucket_name_list)# here index will be generated based on the length of the list provided in the variable
+  bucket = var.bucket_name_list[count.index] # var.bucket_name_list[0] will be the first bucket name, var.bucket_name_list[1] will be the second bucket name and so on
+}
 
-  tags = {
-    Name        = each.key
-    Environment = each.key
-  }
+
+#Bucket Creation usign for_each  meta argument with set of strings
+resource "aws_s3_bucket" "bucket2" {
+  for_each=var.bucket_name_set // here index does not matter as we are using for_each with set of strings, so each bucket name will be treated as a unique key in the for_each loop
+  bucket = each.key // in set of string both key and value will be same as we are using set of strings, so each.key will give us the bucket name directly
 }
--- meta-arguments/variable.tf
+++ meta-arguments/variable.tf
@@ -1,4 +1,12 @@
-variable "envs" {
-  description = "Environments"
-  default = ["dev", "test"]
+variable "bucket_name_list" {
+  description = "List of bucket names to create"
+  type        = list(string)
+  default     = ["counter-meta-argument-1", "counter-meta-argument-2", "counter-meta-argument-3"]
+  
+}
+variable "bucket_name_set" {
+  description = "Set of bucket names to create"
+  type        = set(string)
+  default     = ["for-each-with-set-of-strings-meta-argument-1", "for-each-with-set-of-strings-meta-argument-2", "for-each-with-set-of-strings-meta-argument-3"]
+  
 }
